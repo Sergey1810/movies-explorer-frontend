eb0ab1055b1725a15f66d7aa9ec8e32643c5ecd7
Sergey Lisovcev <deadlook15@yandex.ru>
2023-09-01 21:40:45 +0300

    feat: final

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import buttonIcon from '../../images/headerButtonIcons.svg'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './Navigation.css'
 
 export default function Navigation() {
     const [mobileActive, setMobileActive] = useState(false)
+    const location = useLocation() 
 
     const handleMobileNav = () => {
       setMobileActive(!mobileActive)
@@ -16,8 +17,8 @@ export default function Navigation() {
                 <nav className='navigation__menu'>
                     <ul className="navigation__items">
                     <li className='navigation__item navigation__item_menu-width768_active'><Link to='/' className='navigation__item-link '>Главная</Link></li>
-                        <li className='navigation__item'><Link to='/movies' className='navigation__item-link navigation__item-link_active'>Фильмы</Link></li>
-                        <li className='navigation__item'><Link to='/saved-movies' className='navigation__item-link'>Сохраненные фильмы</Link></li>
+                        <li className='navigation__item'><Link to='/movies' className={`navigation__item-link ${(location.pathname==='/'&&'navigation__item-link-main')} navigation__item-link_active`}>Фильмы</Link></li>
+                        <li className='navigation__item'><Link to='/saved-movies' className={`navigation__item-link ${(location.pathname==='/'&&'navigation__item-link-main')}`}>Сохраненные фильмы</Link></li>
                     </ul>
                     <div className='navigation__button'>
                         <Link to='/profile' className='navigation__button-link'>
